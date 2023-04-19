bac9e8ef56f5e69a999265799d804a53add5a60e
Sun-D-Shine <winthureintun2000@gmail.com>
2023-04-19 19:30:52 +0630

    code in pages/auth.tsx && pages/api/auth/[...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import bcrypt from "bcrypt";
 import NextAuth from "next-auth";
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
@@ -8,6 +10,14 @@ import prisma from "@/libs/prismadb";
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
     CredentialsProvider({
       name: "credentials",
       credentials: {
@@ -16,7 +26,7 @@ export default NextAuth({
       },
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) {
-          throw new Error("Invalid credentials");
+          throw new Error("Email and password required");
         }
 
         const user = await prisma.user.findUnique({
@@ -26,7 +36,7 @@ export default NextAuth({
         });
 
         if (!user || !user?.hashedPassword) {
-          throw new Error("Invalid credentials");
+          throw new Error("Email does not exist");
         }
 
         const isCorrectPassword = await bcrypt.compare(
@@ -35,7 +45,7 @@ export default NextAuth({
         );
 
         if (!isCorrectPassword) {
-          throw new Error("Invalid credentials");
+          throw new Error("Incorrect password");
         }
 
         return user;
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,6 +3,9 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { useCallback, useState } from "react";
 import { signIn } from 'next-auth/react';
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+
 
 const Auth = () => {
   const router = useRouter();
@@ -84,6 +87,16 @@ const Auth = () => {
             <button onClick={variant === "login" ? login : register} className="bg-red-600 py-3 mt-10 text-white hover:bg-red-700 rounded-md w-full transition">
               {variant === "login" ? "Sign In" : "Sign Up"}
             </button>
+
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div onClick={() => signIn('google', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FcGoogle size={32} />
+              </div>
+              <div onClick={() => signIn('github', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FaGithub size={32} />
+              </div>
+            </div>
+
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "New to Netflix?"
