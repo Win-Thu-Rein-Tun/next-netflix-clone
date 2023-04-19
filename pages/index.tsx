a641630e4039be37cc16716d4bdeb5253239bae1
Sun-D-Shine <winthureintun2000@gmail.com>
2023-04-19 23:41:49 +0630

    code in pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
 
 export default function Home() {
   return (
-    <><h1 className="text-2xl text-green-500">
-      Hello Next
-    </h1>
+    <>
+      <h1 className="text-8xl text-green-500">Hello Next</h1>
+      <button onClick={() => signOut()} className="bg-white w-full">
+        Sign Out
+      </button>
     </>
   );
 }
