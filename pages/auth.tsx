a86e324bab14cd20c1012a505ae1af77b7b07908
Sun-D-Shine <winthureintun2000@gmail.com>
2023-04-19 12:35:32 +0630

    create & code in components/input

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,3 +1,5 @@
+import Input from "@/components/Input";
+
 const Auth = () => {
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat ">
@@ -6,7 +8,12 @@ const Auth = () => {
           <img src="/images/logo.png" className="h-12" alt="Logo" />
         </nav>
         <div className="flex justify-center">
-          <div className="bg-black">Sign in</div>
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white font-semibold text-4xl mb-8">Sign in</h2>
+            <div className="flex flex-col gap-4">
+              <Input/>
+            </div>
+          </div>
         </div>
       </div>
     </div>
