612648f4e62e1a6e02ce07ad1ab44de2a24b719d
Sun-D-Shine <winthureintun2000@gmail.com>
2023-04-20 12:13:32 +0630

    code in components/Navbar&NavbarItem.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import NavbarItem from './NavbarItem'
+import React from "react";
+import NavbarItem from "./NavbarItem";
+import { BsChevronDown } from "react-icons/bs";
+
 const Navbar = () => {
   return (
-    <nav className='w-full fixed z-40'>
-        <div className='px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 '>
-            <img className='h-4 lg:h-7' src="/images/logo.png" alt="logo" />
-            <div className="flex-row ml-8 gap-7 hidden lg:flex">
-                <NavbarItem/>
-            </div>
+    <nav className="w-full fixed z-40">
+      <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ">
+        <img className="h-4 lg:h-7" src="/images/logo.png" alt="logo" />
+        <div className="flex-row ml-8 gap-7 hidden lg:flex">
+          <NavbarItem label="Home" active />
+          <NavbarItem label="Series" />
+          <NavbarItem label="Films" />
+          <NavbarItem label="New & Popular" />
+          <NavbarItem label="My List" />
+          <NavbarItem label="Browse by Languages" />
         </div>
+        <div className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
+          <p className="text-white text-sm">Browse</p>
+          <BsChevronDown className={`w-4 text-white fill-white transition `} />
+          <MobileMenu />
+        </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
